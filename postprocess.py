efbad64480766d98a180371c393240d918c70788
Sylux6 <sy-lux@hotmail.fr>
2018-03-14 11:01:22 +0100

    [update] histograms generation

--- postprocess.py
+++ postprocess.py
@@ -35,22 +35,66 @@ begin_time = timelib.time()
 ###########
 # CONSTANTS
 filename = "Outputs/Mini_sessions.csv"
-requests_threshold = 20
+requests_threshold = 6
+parameters = ["requests","timespan","requested_category_richness","requested_my_thema_richness","star_chain_like","bifurcation","entropy","variance","popularity_mean","inter_req_mean_seconds","TV_proportion","Celebrities_proportion","Series_proportion","Movies_proportion","Music_proportion","Unclassifiable_proportion","Comic_proportion","VideoGames_proportion","Other_proportion","Sport_proportion","News_proportion","read_pages"]
+log_scale_parameters = ["requests", "timespan", "inter_req_mean_seconds"]
 
 ####################
 # READING DATA FILES 
 start_time = timelib.time()
-sessions = pd.read_csv(filename, sep=',', dtype='object', na_filter=False)
-
-############################################################################################################
-# TYPE CASTING (read csv from previously generated file session import data in str type, annoying isn't it?)
-sessions.requests = sessions.requests.apply(lambda x: int(x))
+print("\n   * Loading "+filename+" ...", end="\r")
+sessions = pd.read_csv(filename, sep=',')
+print("   * "+filename+" loaded ({} rows) in {:.1f} seconds.".format(sessions.shape[0], timelib.time()-start_time))
+sessions.fillna(0, inplace=True)
 
 ###############
 # NORMALIZATION
-sessions["normalized_requests"] = normalize(sessions.requests)
+start_time = timelib.time()
+normalized_parameters = []
+print("\n   * Normalizing parameters ...", end="\r")
+for p in parameters:
+    normalized_parameters.append("normalized_"+p)
+    sessions["normalized_"+p] = normalize(sessions[p])
+print("   * Parameters normalized in %.1f seconds." %(timelib.time()-start_time))
 
-print(sessions[sessions.requests.values>requests_threshold][["requests", "normalized_requests"]])
+sessions = sessions[sessions.requests>requests_threshold]
+
+
+#######################
+# GENERATING HISTOGRAMS
+start_time = timelib.time()
+print("\n   * Generating histograms ...")
+for p in parameters:
+    # regular histograms
+    print("        Generating Matplot/"+p+".pdf ...", end="\r")
+    plt.hist(sessions[p].values, align="left")
+    plt.xlabel(p)
+    plt.ylabel("Frequency")
+    if p in log_scale_parameters:
+        plt.gca().set_xscale('log')
+        plt.gca().set_yscale('log')
+    plt.savefig("Matplot/"+p+".pdf",format='pdf')
+    print("        Matplot/"+p+".pdf successfully generated")
+    plt.clf()
+    # normalized histograms
+    print("        Generating Matplot/Normalized/normalized_"+p+".pdf ...", end="\r")
+    plt.hist(sessions["normalized_"+p].values, align="left")
+    plt.xlabel("Normalized "+p)
+    plt.ylabel("Frequency")
+    if p in log_scale_parameters:
+        plt.gca().set_xscale('log')
+        plt.gca().set_yscale('log')
+    plt.savefig("Matplot/Normalized/normalized_"+p+".pdf",format='pdf')
+    print("        Matplot/Normalized/normalized_"+p+".pdf successfully generated")
+    plt.clf()
+print("   * Histograms generated in %.1f seconds." %(timelib.time()-start_time))
+
+##########################
+# GENERATING SCATTER PLOTS
+start_time = timelib.time()
+print("\n   * Generating scatter plots ...")
+# TODO
+print("   * Scatter plots generated in %.1f seconds." %(timelib.time()-start_time))
 
 ###############
 # END OF SCRIPT
